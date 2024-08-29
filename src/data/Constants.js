a650c8650299c39609af8c9900590681cd730309
Mashookh Khan <mashooq.ahmed175@gmail.com>
2024-08-29 16:43:38 +0530

    resume update

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -10,7 +10,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/MashookhKhanlol",
     resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+      "https://drive.google.com/file/d/1zeBxsUjhim7KXDvawRPceNKNvE0IcgtM/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/mashookh-khan-5a8a1024b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     twitter: "https://x.com/UnderDo65762290?t=pKEg7pVAju8qZC-ARnzVow&s=08",
     facebook: "https://www.facebook.com/share/7PUbhu1deu3dXu9r/?mibextid=qi2Omg",
